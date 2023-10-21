c08cc1169addb001242baf7b643f5cabf62dae39
Aboudourazakou <tetereou2017@gmail.com>
2023-10-21 15:38:16 +0200

    small changes

--- src/parser/DotParser.java
+++ src/parser/DotParser.java
@@ -152,7 +152,7 @@ public class DotParser {
 
         DotParser c = new DotParser();
         try {
-            c.parseDotFile("C:\\Users\\teter\\OneDrive\\Bureau\\ModelChecker\\automata.dot");
+            c.parseDotFile("PathToDotFile");
         }catch (IOException e){}
 
     }
