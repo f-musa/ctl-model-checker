3564044be00f0b10bc8fe7c02facf4638095bd06
Moussa Fall <fallmoussa98xo@gmail.com>
2023-11-01 16:09:31 +0100

    modify algorithms and add EX implementation

--- src/parser/algo/Algorithms.java
+++ src/parser/algo/Algorithms.java
@@ -5,79 +5,82 @@ import parser.dot.*;
 
 public class Algorithms {
 
-    public void markAllStates (Formula formula,Automata automata,Boolean value)
-    {
-        automata.getStates().forEach(state ->{
-            state.getMarkings().put(formula, value);
-        });
-    }
+   
     public void markAtomic(Formula formula , Automata automata )
     {
         automata.getStates().forEach(state -> {
             if(formula instanceof Atomic){ 
-                if(state.getLabels().contains(((Atomic)formula).getAtomicProposition())){
+                if(!state.getMarkings().get(formula)){
+                    if(state.getLabels().contains(((Atomic)formula).getAtomicProposition())){
                     state.getMarkings().put(formula, true);
-                }
+                    }
                 else
                     state.getMarkings().put(formula, false);
-            }   
+                }   
+                
+        }
+        
         })
         ;
     }
     public void markFormula(Formula formula, Automata automata){
        
+
         
-        automata.getStates().forEach(state ->{
             if(formula instanceof Atomic){
                 markAtomic(formula, automata);
             }
-        
+
             else if(formula instanceof Not){
                 Formula innerFormula = ((Not)formula).getFormula();
-
-                
-                if(!(state.getMarkings().get(innerFormula)))
-                    markFormula(innerFormula,automata);
-                
-                Boolean value = state.getMarkings().get(innerFormula);
-                markAllStates(formula, automata, !value);
+                markFormula(innerFormula, automata);
+                automata.getStates().forEach(state->{
+                   Boolean value =  state.getMarkings().get(innerFormula);
+                   if(!state.getMarkings().get(formula))
+                        state.getMarkings().put(formula,!value);
+                });
+            
             }
+
             else if(formula instanceof And){
                 
                 Formula leftFormula = ((And)formula).getLetFormula();
                 Formula rightFormula = ((And)formula).getRightFormula();
-
-                if(!(state.getMarkings().get(leftFormula)))
-                    markFormula(leftFormula,automata);
-                
-                if(!(state.getMarkings().get(rightFormula)))
-                    markFormula(rightFormula,automata);
-                
-                Boolean leftValue = state.getMarkings().get(leftFormula);
-                Boolean rightValue = state.getMarkings().get(rightFormula);
-                markAllStates(formula, automata, leftValue && rightValue);
+                markFormula(leftFormula,automata);                
+                markFormula(rightFormula,automata);
+                automata.getStates().forEach(state->{
+                    Boolean leftValue = state.getMarkings().get(leftFormula);
+                    Boolean rightValue = state.getMarkings().get(rightFormula);
+                    if(!state.getMarkings().get(formula))
+                        state.getMarkings().put(formula,leftValue&&rightValue); 
+                });
             }
             else if(formula instanceof Or){
-                
                 Formula leftFormula = ((Or)formula).getLetFormula();
                 Formula rightFormula = ((Or)formula).getRightFormula();
-
-                if(!(state.getMarkings().get(leftFormula)))
-                    markFormula(leftFormula,automata);
-                
-                if(!(state.getMarkings().get(rightFormula)))
-                    markFormula(rightFormula,automata);
-                
-                Boolean leftValue = state.getMarkings().get(leftFormula);
-                Boolean rightValue = state.getMarkings().get(rightFormula);
-                markAllStates(formula, automata, leftValue || rightValue);
+                markFormula(leftFormula,automata);                
+                markFormula(rightFormula,automata);
+                automata.getStates().forEach(state->{
+                    Boolean leftValue = state.getMarkings().get(leftFormula);
+                    Boolean rightValue = state.getMarkings().get(rightFormula);
+                    if(!state.getMarkings().get(formula))
+                        state.getMarkings().put(formula,leftValue||rightValue); 
+                });
             }
             else if(formula instanceof EX){
-                Formula inneFormula = ((EX) formula).getFormula();
-                
+                Formula innerFormula = ((EX) formula).getFormula();
+                markFormula(innerFormula, automata);
+                automata.getStates().forEach(state->{
+                    state.getMarkings().put(formula,false); 
+                });
+                automata.getTransitions().forEach(transition->{
+                    if(transition.getFrom().getMarkings().get(innerFormula)==true){
+                        transition.getTo().getMarkings().put(formula, true);
+                        ((EX) formula).setIsVerified(true);
+                    }
+                });
             }
                
-        });
     }
 }
 
